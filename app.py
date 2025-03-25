53f59e9db7e7562a800822cb33bfce20116f002d
jojocoder28 <dasjojo7@gmail.com>
2025-03-25 23:44:40 +0530

    classification

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ star = st.sidebar.selectbox("Star", new_df['star'].unique())
 country = st.sidebar.selectbox("Country", new_df['country'].unique())
 company = st.sidebar.selectbox("Company", new_df['company'].unique())
 rating = st.sidebar.selectbox("Rating", new_df['rating'].unique())
-runtime = st.sidebar.slider("Runtime (in minutes)", df['runtime'].min(),df['runtime'].max(),df['runtime'].median())
-votes = st.sidebar.slider("Votes (in thousands)", df['votes'].min(),df['votes'].max(),df['votes'].median())
+runtime = st.sidebar.slider("Runtime (in minutes)", df['runtime'].min(),df['runtime'].max(),int(df['runtime'].median()))
+votes = st.sidebar.slider("Votes (in thousands)", df['votes'].min(),df['votes'].max(),int(df['votes'].median()))
 budget = st.sidebar.number_input("Budget (in millions)", min_value=df['budget'].min(),max_value=df['budget'].max(), value=100)
 year = st.sidebar.number_input("Year", min_value=1900, max_value=2025, value=2020)  # Adding 'year' feature
 
